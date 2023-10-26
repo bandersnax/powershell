1efb603d6f2586dafa6308dd7b9cad3362268c20
Ken Meservy <mcswervy@fastmail.com>
2023-10-26 15:14:13 -0400

    comments

--- search_rewards/get_points.ps1
+++ search_rewards/get_points.ps1
@@ -5,16 +5,17 @@
 
 $response = (Invoke-RestMethod -Uri "https://copylists.com/downloads/words/nouns/nouns.json")
 
-# Pull 30 items out of the response I mistakenly thought that you couldn't do this but I was just
-# using the wrong parameter for Get-Random - you can just pipe the noun property right into it without
-# explicitly making it an array with $response.noun.split([System.Environment]::NewLine)
+# Pull 30 items out of the response 
+## I mistakenly thought that you couldn't do this but I was just using the wrong parameter 
+## for Get-Random - you can just pipe the noun property right into it without explicitly
+## making it an array with $response.noun.split([System.Environment]::NewLine)
 
 $nouns = $response.noun | Get-Random -Count 30
 
 # Loop through the search terms, opening a page in bing for each one
+## The default behavior could be improved quite a bit here - this will open however many 
+## terms you're searching for as new tabs in a current open edge window
 
-foreach( $search_term in $nouns) {
-    Start-Process microsoft-edge:https://www.bing.com/search?q=${search_term}
+foreach( $searchTerm in $nouns) {
+    Start-Process microsoft-edge:https://www.bing.com/search?q=${searchTerm}
 }
-
-
